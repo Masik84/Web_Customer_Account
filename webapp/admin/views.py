1cd77426b5a68ce38df8335659ce8df2fdffc76f
Masik84 <mouse1984@mail.ru>
2022-06-17 18:17:17 +0300

    admin_pages created

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,19 +1,37 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import current_user
+import imp
+from flask import Blueprint, redirect, render_template, url_for
+from flask_login import logout_user
+from webapp.user.views import logout
 
 from webapp.db import db
 from webapp.user.decorators import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
+
 @blueprint.route('/')
-#@admin_required
+@admin_required
 def admin_index():
-    title = 'Панель управления'
+    title = 'Control Panel'
     return render_template('admin/index.html', page_title=title)
 
 
-@blueprint.route('/products')
-def group_btn_clcked():
-    if request.method == 'POST'
-    return render_template('admin.products.html')
+@blueprint.route("/users")
+@admin_required
+def users_page():
+    return render_template("admin/users.html")
+
+
+@blueprint.route("/customers")
+def customers_page():
+    return render_template("admin/customers.html")
+
+
+@blueprint.route("/managers")
+def managers_page():
+    return render_template("admin/managers.html")
+
+
+@blueprint.route("/products")
+def products_page():
+    return render_template("admin/products.html")
